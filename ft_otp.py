ca70fc21a82885ea001260c18dfc4ffc98b49f00
Brian Riveiro Del Rio <briveiro@c3r5s4.42malaga.com>
2023-05-05 13:36:48 +0200

    99%

--- ft_otp.py
+++ ft_otp.py
@@ -1,18 +1,21 @@
-import time, hashlib, argparse
+import time, hashlib, argparse, os, base64
 
 def getArgs():
 	getter = argparse.ArgumentParser(description="Tool for generate a one time password.")
 	getter.add_argument('-g', type=str,)
 	getter.add_argument('-k', type=str)
 	return(getter.parse_args())
-
 def optionk(b32):
       now = int(time.time() // 30)
       converter = hashlib.sha256(str(now).encode('utf-8'))
       hexconvert = converter.hexdigest()[:6]
       result = int(hexconvert, base=16) % 1000000
       print(result)
-  
+
+# def decode(passwd):
+#       pswdsha256 = hashlib.sha256(passwd[32:].encode('utf-8')).hexdigest()
+#       pswdmd5 = hashlib.md5(pswdsha256[:32].encode('utf-8')).hexdigest()
+
 def optiong(fdin):
       try:
             opener = open(fdin, 'r')
@@ -23,11 +26,13 @@ def optiong(fdin):
             quit("Error: could not read {}".format(fdin))
       if len(passwd) < 64:
             quit("Error: Your password must have 64 characters and you send {}".format(len(passwd)))
-      pswdsha256 = hashlib.sha256(passwd.encode('utf-8')).hexdigest()
-      pswdmd5 = hashlib.md5(pswdsha256.encode('utf-8')).hexdigest()
+      encr = os.urandom(16)
+      pswdsha256 = hashlib.sha256(encr + passwd.encode('utf-8')).digest()
+      ecript = base64.b64encode(encr + pswdsha256).decode('utf-8')
+
       try:
             with open("ft_otp.key", 'w') as openout:
-                  openout.write(pswdmd5 + pswdsha256)
+                  openout.write(ecript)
             quit("Key was successfully saved in ft_otp.key")
       except Exception as e:
             quit("Error: {}".format(e))
